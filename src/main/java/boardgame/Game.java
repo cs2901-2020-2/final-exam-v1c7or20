137d2711e5f93f51a63bc6237240a2c0b0d57330
victor <victor.pena@utec.edu.oe>
2020-12-23 12:42:35 -0500

    added points

--- src/main/java/boardgame/Game.java
+++ src/main/java/boardgame/Game.java
@@ -4,7 +4,7 @@ public class Game {
     public static void main(String[] args) {
         Application application = new Application();
         Board board = new Board();
-        System.out.print("Empezando el juego");
+        System.out.print("Empezando el juego\n");
         while (!board.verifyEnd()){
             application.playGame(board);
         }
--- src/test/java/boardgame/Tests.java
+++ src/test/java/boardgame/Tests.java
@@ -64,4 +64,6 @@ public class Tests {
         Assert.assertEquals(board.getPoints(), 120);
 
     }
+
+
 }
