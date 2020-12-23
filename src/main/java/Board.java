d1eec2a9c7044b76988a62d34cee1568d0d0e02d
victor <victor.pena@utec.edu.oe>
2020-12-23 10:36:11 -0500

    added pieces and Application

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,3 +1,12 @@
 public class Board {
+    private char [][]board;
 
+    public Board(){
+        board = new char[9][9];
+    }
+
+    public void playPiece(Piece piece, int cord){
+        if (cord < 9){
+        }
+    }
 }
